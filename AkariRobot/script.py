f7b85460921b32b548064b7074e2c6bc7419bf7e
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-01-16 15:07:17 +0530

    Add files via upload

---
+++ AkariRobot/script.py
@@ -0,0 +1,7 @@
+SHIKI_IMG = (
+      "https://telegra.ph/file/562b4951e8f1d45e6e297.jpg",
+      "https://telegra.ph/file/b4990dc110e4b1565f4a6.jpg",
+      "https://telegra.ph/file/13e6b986e95d613905b42.jpg",
+      "https://telegra.ph/file/3b626f0a8f33937378a14.jpg",
+      "https://telegra.ph/file/086dceeee5388e37384a0.jpg",
+)
