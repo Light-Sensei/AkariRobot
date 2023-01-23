e15092663b60d79bfa3042d84008983f1749c78b
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-01-23 22:48:23 +0530

    Update alive.py

--- AkariRobot/modules/alive.py
+++ AkariRobot/modules/alive.py
@@ -9,16 +9,16 @@ from AkariRobot.events import register
 from AkariRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/bd9ce6e2c66833236248d.mp4"
+PHOTO = "https://telegra.ph/file/7ef542475e2b5962144d1.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"やあ Kon'ichiwa [{event.sender.first_name}](tg://user?id={event.sender.id})-San, Watashi Wa Shikimori Des. \n\n"
+  TEXT = f"やあ Kon'ichiwa [{event.sender.first_name}](tg://user?id={event.sender.id})-San, Am Akari Watanabe. \n\n"
   TEXT += "♡ Am Functioning Properly Duh ♡ \n\n"
-  TEXT += f"♡ My Darling : [Light Yagami♡ (夜神月♡)](t.me/yagami_roito)** \n\n"
+  TEXT += f"♡ My Hubby : [Light Yagami♡ (夜神月♡)](t.me/yagami_roito)\n\n"
   TEXT += f"♡ Library Version : `{telever}` \n"
   TEXT += f"♡ Telethon Version : `{tlhver}` \n"
   TEXT += f"♡ Pyrogram Version : `{pyrover}` \n"
-  TEXT += "♡ Arigato For Adding me Here ♡"
-  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/ShikimoriXupdates"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/shikimoriXsupport")]]
+  TEXT += "♡ Thanks For Adding me Here ♡"
+  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/AkariWatanabeupdates"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/shikimoriXsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
