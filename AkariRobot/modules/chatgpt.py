52470295f4d106cbcc56bb3205b5ae98b2b568f5
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-06-09 12:58:05 +0530

    Create chatgpt.py

---
+++ AkariRobot/modules/chatgpt.py
@@ -0,0 +1,68 @@
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import openai
+
+# Set up OpenAI API credentials
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+# Set up Telegram bot credentials
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Define a function to handle /chatgpt command
+def chatgpt(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Akari's Chatgpt. How can I assist you?")
+
+# Define a function to handle incoming messages
+def message_handler(update: Update, context):
+    # Get the user's message
+    user_message = update.message.text
+    
+    # Call the ChatGPT model
+    response = chat_with_gpt(user_message)
+    
+    # Send the response back to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+# Function to interact with ChatGPT
+def chat_with_gpt(user_message):
+    # Call OpenAI API to get a response
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=user_message,
+        max_tokens=50,
+        temperature=0.7,
+        n=1,
+        stop=None,
+        context=None,
+        log_level=None,
+    )
+    
+    # Extract and return the response
+    return response.choices[0].text
+
+# Set up the Telegram bot
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Define the command handlers
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # Define the message handler
+    message_handler = MessageHandler(Filters.text & ~Filters.command, message_handler)
+    dispatcher.add_handler(message_handler)
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == 'chatgpt.py':
+    chatgpt.py()
