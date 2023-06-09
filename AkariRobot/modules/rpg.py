b8b7c65c1de5aae4c96c601360109bb215895ada
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-06-09 22:51:28 +0530

    Update rpg.py

--- AkariRobot/modules/rpg.py
+++ AkariRobot/modules/rpg.py
@@ -20,227 +20,53 @@ from AkariRobot.modules.disable import DisableAbleCommandHandler
 from AkariRobot.modules.helper_funcs.alternate import typing_action
 from AkariRobot.modules.helper_funcs.chat_status import callbacks_in_filters
 
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.level = 1
-        self.gold = 0
-        self.inventory = []
-        self.messages_sent = 0
 
-    def add_gold(self, amount):
-        self.gold += amount
-
-    def add_item_to_inventory(self, item):
-        self.inventory.append(item)
-
-def level_up(character):
-    if character.messages_sent % 200 == 0:
-        character.level += 1
-
-# Command handlers...
 
 def playrpg(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="wanna play rpg click /choose!")
-
-def help(update, context):
-    help_text = """
-    Available commands:
-    /playrpg - Start the bot
-    /choose - Choose a character
-    /help - Show the help message
-    /create - Create a character
-    /fight - Fight an enemy
-    /balance - Check your balance
-    /shop - Visit the shop
-    /inventory - View your inventory
-    /daily - Claim daily reward
-    /weekly - Claim weekly reward
-    /buy - Buy an item from the shop
-    /gift - Gift an item to another player
-    /marry - Get married (requires level 10)
-    /build - Build an item (e.g., /build house)
-    /hunt - Hunt an animal
-    /sell - Sell a hunted animal
-    """
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
-
-def choose(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Male", callback_data='choose_male'),
-         InlineKeyboardButton("Female", callback_data='choose_female')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Please choose a character.", reply_markup=reply_markup)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the RPG bot! Use /create to start playing.")
 
 def create(update, context):
-    args = context.args
-    if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a name for your character.")
-    else:
-        name = ' '.join(args)
-        character = Character(name)
-        context.user_data['character'] = character
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Character {name} created successfully!")
-
-def fight(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You are fighting an enemy!")
-
-def balance(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        balance_text = f"Your current balance is {character.gold} gold coins."
-        context.bot.send_message(chat_id=update.effective_chat.id, text=balance_text)
-
-def shop(update, context):
     keyboard = [
-        [InlineKeyboardButton("Item 1", callback_data='buy_item1'),
-         InlineKeyboardButton("Item 2", callback_data='buy_item2')],
-        [InlineKeyboardButton("Item 3", callback_data='buy_item3'),
-         InlineKeyboardButton("Item 4", callback_data='buy_item4')]
+        [InlineKeyboardButton("Male", callback_data='male')],
+        [InlineKeyboardButton("Female", callback_data='female')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the shop!", reply_markup=reply_markup)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Choose your gender:", reply_markup=reply_markup)
 
-def inventory(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        inventory_text = f"Your inventory: {', '.join(character.inventory)}"
-        context.bot.send_message(chat_id=update.effective_chat.id, text=inventory_text)
+def create_character(update, context):
+    query = update.callback_query
+    gender = query.data
+    query.answer()
+    context.bot.send_message(chat_id=query.message.chat_id, text=f"You selected: {gender}")   
 
 def daily(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You claimed your daily reward!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You received your daily reward!")
 
 def weekly(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You claimed your weekly reward!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You received your weekly reward!")
 
-def buy(update, context):
-    args = context.args
-    if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to buy.")
-    else:
-        item_name = ' '.join(args)
-        character = context.user_data.get('character')
-        if character is None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-        else:
-            # Prompt for selecting the price
-            keyboard = [
-                [InlineKeyboardButton("10 gold", callback_data=f'buy_price_{item_name}_10'),
-                 InlineKeyboardButton("20 gold", callback_data=f'buy_price_{item_name}_20')],
-                [InlineKeyboardButton("30 gold", callback_data=f'buy_price_{item_name}_30'),
-                 InlineKeyboardButton("Cancel", callback_data='buy_cancel')]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Please select the price:", reply_markup=reply_markup)
-
-def process_buy_callback(update, context):
-    query = update.callback_query
-    query.answer()
-    query_data = query.data
-
-    if query_data == 'buy_cancel':
-        context.bot.send_message(chat_id=query.message.chat_id, text="Purchase canceled.")
-    elif query_data.startswith('buy_price_'):
-        item_data = query_data.split('_')
-        item_name = item_data[2]
-        price = item_data[3]
-        character = context.user_data.get('character')
-        if character is not None:
-            character.add_item_to_inventory(item_name)
-            character.add_gold(int(price))
-            context.bot.send_message(chat_id=query.message.chat_id, text=f"You bought {item_name} for {price} gold coins!")
-
-def gift(update, context):
-    args = context.args
-    if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a player name to gift the item to.")
-    elif len(args) == 1:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to gift.")
-    else:
-        player_name = args[0]
-        item_name = ' '.join(args[1:])
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You gifted {item_name} to {player_name}!")
-
-def marry(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    elif character.level < 10:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You need to be at least level 10 to get married.")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Congratulations! You got married!")
-
-def build(update, context):
-    args = context.args
-    if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to build.")
-    else:
-        item_name = ' '.join(args)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You built a {item_name}!")
-
-def hunt(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="You are hunting...")
-
-def sell(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="You sold an animal!")
-
-def error(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Oops! Something went wrong.")
-
-# Set up the bot...
+def balance(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Your current balance is: $100")
 
 def main():
-    # Initialize the bot
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    playrpg_handler = CommandHandler('playrpg', playrpg)
+    create_handler = CommandHandler('create', create)
+    create_character_handler = CallbackQueryHandler(create_character)
+    daily_handler = CommandHandler('daily', daily)
+    weekly_handler = CommandHandler('weekly', weekly)
+    balance_handler = CommandHandler('balance', balance)
 
-    # Add command handlers
-    dp.add_handler(CommandHandler("playrpg", playrpg))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("choose", choose))
-    dp.add_handler(CommandHandler("create", create))
-    dp.add_handler(CommandHandler("fight", fight))
-    dp.add_handler(CommandHandler("balance", balance))
-    dp.add_handler(CommandHandler("shop", shop))
-    dp.add_handler(CommandHandler("inventory", inventory))
-    dp.add_handler(CommandHandler("daily", daily))
-    dp.add_handler(CommandHandler("weekly", weekly))
-    dp.add_handler(CommandHandler("buy", buy))
-    dp.add_handler(CommandHandler("gift", gift))
-    dp.add_handler(CommandHandler("marry", marry))
-    dp.add_handler(CommandHandler("build", build))
-    dp.add_handler(CommandHandler("hunt", hunt))
-    dp.add_handler(CommandHandler("sell", sell))
+    dispatcher.add_handler(playrpg_handler
+    dispatcher.add_handler(create_handler)
+    dispatcher.add_handler(create_character_handler)
+    dispatcher.add_handler(daily_handler)
+    dispatcher.add_handler(weekly_handler)
+    dispatcher.add_handler(balance_handler)
 
-    # Add callback handlers
-    dp.add_handler(CallbackQueryHandler(process_buy_callback))
-
-    # Add error handler
-    dp.add_error_handler(error)
-
-    # Start the bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
     updater.idle()
 
 if __name__ == '__main__':
