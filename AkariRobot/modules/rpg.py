ce546ea956175db1982f2baa952e471cdadd367c
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-06-09 14:09:03 +0530

    Update rpg.py

--- AkariRobot/modules/rpg.py
+++ AkariRobot/modules/rpg.py
@@ -2,8 +2,6 @@ import random
 from datetime import datetime, timedelta
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Character class and other code...
-
 class Character:
     def __init__(self, name):
         self.name = name
@@ -24,13 +22,13 @@ def level_up(character):
 
 # Command handlers...
 
-def startrpg(update, context):
+def play_rpg(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the RPG Bot!")
 
 def help(update, context):
     help_text = """
     Available commands:
-    /startrpg - Start the bot
+    /playrpg - Start the bot
     /choose - Choose a character
     /help - Show the help message
     /create - Create a character
@@ -93,138 +91,76 @@ def daily(update, context):
     if character is None:
         context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
     else:
-        last_daily_claim = context.user_data.get('last_daily_claim')
-        if last_daily_claim is not None and datetime.now() - last_daily_claim < timedelta(days=1):
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You have already claimed your daily reward.")
-        else:
-            # Give the daily reward
-            reward = 50  # Set the daily reward amount
-            character.add_gold(reward)
-            context.user_data['last_daily_claim'] = datetime.now()
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You claimed your daily reward of {reward} gold coins.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You claimed your daily reward!")
 
 def weekly(update, context):
     character = context.user_data.get('character')
     if character is None:
         context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
     else:
-        last_weekly_claim = context.user_data.get('last_weekly_claim')
-        if last_weekly_claim is not None and datetime.now() - last_weekly_claim < timedelta(weeks=1):
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You have already claimed your weekly reward.")
-        else:
-            # Give the weekly reward
-            reward = 200  # Set the weekly reward amount
-            character.add_gold(reward)
-            context.user_data['last_weekly_claim'] = datetime.now()
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You claimed your weekly reward of {reward} gold coins.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You claimed your weekly reward!")
 
 def buy(update, context):
     args = context.args
     if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item to buy.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to buy.")
     else:
         item_name = ' '.join(args)
         character = context.user_data.get('character')
         if character is None:
             context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
         else:
-            # Check if the item is available in the shop and deduct the gold
-            item_cost = 50  # Set the cost of the item
-            if character.gold >= item_cost:
-                character.gold -= item_cost
-                character.add_item_to_inventory(item_name)
-                context.bot.send_message(chat_id=update.effective_chat.id, text=f"You bought {item_name} for {item_cost} gold coins.")
-            else:
-                context.bot.send_message(chat_id=update.effective_chat.id, text="Insufficient gold.")
+            character.add_item_to_inventory(item_name)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You bought {item_name}!")
 
 def gift(update, context):
     args = context.args
-    if len(args) < 2:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide the recipient and item to gift.")
+    if len(args) == 0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a player name to gift the item to.")
+    elif len(args) == 1:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to gift.")
     else:
-        recipient = args[0]
+        player_name = args[0]
         item_name = ' '.join(args[1:])
-        character = context.user_data.get('character')
-        if character is None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-        else:
-            if item_name in character.inventory:
-                character.inventory.remove(item_name)
-                context.bot.send_message(chat_id=update.effective_chat.id, text=f"You gifted {item_name} to {recipient}.")
-            else:
-                context.bot.send_message(chat_id=update.effective_chat.id, text="You don't have that item to gift.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You gifted {item_name} to {player_name}!")
 
 def marry(update, context):
     character = context.user_data.get('character')
     if character is None:
         context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
+    elif character.level < 10:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You need to be at least level 10 to get married.")
     else:
-        level = character.level  # Replace with the appropriate way to get the character's level
-        if level >= 10:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Congratulations! You are now married.")
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You need to be at least level 10 to get married.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Congratulations! You got married!")
 
 def build(update, context):
     args = context.args
     if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide the item to build.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide an item name to build.")
     else:
         item_name = ' '.join(args)
-        character = context.user_data.get('character')
-        if character is None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-        else:
-            if item_name == "house":
-                house_cost = 100  # Set the cost of building a house
-                if character.gold >= house_cost:
-                    character.gold -= house_cost
-                    character.add_item_to_inventory("House")
-                    context.bot.send_message(chat_id=update.effective_chat.id, text="You have built a house!")
-                else:
-                    context.bot.send_message(chat_id=update.effective_chat.id, text="Insufficient gold.")
-            else:
-                context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid item to build.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You built a {item_name}!")
 
 def hunt(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        animal_list = ["rabbit", "deer", "wolf"]  # List of animals to hunt
-        animal = random.choice(animal_list)
-        context.user_data['animal'] = animal
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You are hunting a {animal}!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You are hunting...")
 
 def sell(update, context):
-    character = context.user_data.get('character')
-    if character is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
-    else:
-        animal = context.user_data.get('animal')
-        if animal is None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't hunted any animal yet. Use /hunt to hunt an animal.")
-        else:
-            # Sell the hunted animal and add gold to the character
-            animal_price = {'rabbit': 10, 'deer': 20, 'wolf': 30}  # Set the prices for each animal
-            price = animal_price.get(animal, 0)
-            character.add_gold(price)
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You sold the {animal} for {price} gold coins.")
-            context.user_data.pop('animal')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You sold an animal!")
 
-def message_handler(update, context):
-    character = context.user_data.get('character')
-    if character is not None:
-        character.messages_sent += 1
-        level_up(character)
+def error(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Oops! Something went wrong.")
 
-# Create the updater and add handlers...
+# Set up the bot...
 
 def main():
-    updater = Updater("5641185391:AAH3YaOJWxfPDqu4m8bWRM-tnhhBEcQ0KoU", use_context=True)
+    # Initialize the bot
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
+    # Add command handlers
+    dp.add_handler(CommandHandler("playrpg", play_rpg))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("choose", choose))
     dp.add_handler(CommandHandler("create", create))
@@ -241,15 +177,14 @@ def main():
     dp.add_handler(CommandHandler("hunt", hunt))
     dp.add_handler(CommandHandler("sell", sell))
 
-    dp.add_handler(MessageHandler(Filters.text, message_handler))
+    # Add error handler
+    dp.add_error_handler(error)
 
+    # Start the bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
     updater.idle()
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
