b8c3b7b1c758e0f7031b732d456bcd35e5c5a040
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-06-09 14:39:47 +0530

    done

--- AkariRobot/modules/rpg.py
+++ AkariRobot/modules/rpg.py
@@ -1,6 +1,35 @@
 import random
 from datetime import datetime, timedelta
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telethon import TelegramClient, events
+from telethon.tl.types import MessageEntityMention
+import random
+
+# Your API credentials
+api_id = 'API_ID'
+api_hash = 'API_HASH'
+
+# Initialize the Telegram client
+client = TelegramClient('rpg_session', api_id, api_hash)
+
+# RPG game commands
+@client.on(events.NewMessage(pattern='/playrpg'))
+async def handle_playrpg(event):
+    # Check if the message was sent in a group
+    if event.is_group:
+        # Get the mentioned user ID
+        entities = event.message.entities
+        if entities and isinstance(entities[0], MessageEntityMention):
+            user_id = entities[0].user_id
+            # Start the RPG game logic
+            await event.reply(f'Starting RPG game with user press /create {user_id}')
+            # Implement your RPG game logic here
+            # You can interact with the mentioned user using the user_id
+
+# Start the client
+client.start()
+client.run_until_disconnected()
 
 class Character:
     def __init__(self, name):
@@ -22,11 +51,9 @@ def level_up(character):
 
 # Command handlers...
 
-def play_rpg(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the RPG Bot!")
 
-def help(update, context):
-    help_text = """
+def helprpg(update, context):
+    helprpg_text = """
     Available commands:
     /playrpg - Start the bot
     /choose - Choose a character
@@ -48,7 +75,12 @@ def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
 
 def choose(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Please choose a character.")
+    keyboard = [
+        [InlineKeyboardButton("Male", callback_data='choose_male'),
+         InlineKeyboardButton("Female", callback_data='choose_female')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please choose a character.", reply_markup=reply_markup)
 
 def create(update, context):
     args = context.args
@@ -76,7 +108,14 @@ def balance(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text=balance_text)
 
 def shop(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the shop!")
+    keyboard = [
+        [InlineKeyboardButton("Item 1", callback_data='buy_item1'),
+         InlineKeyboardButton("Item 2", callback_data='buy_item2')],
+        [InlineKeyboardButton("Item 3", callback_data='buy_item3'),
+         InlineKeyboardButton("Item 4", callback_data='buy_item4')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the shop!", reply_markup=reply_markup)
 
 def inventory(update, context):
     character = context.user_data.get('character')
@@ -110,8 +149,32 @@ def buy(update, context):
         if character is None:
             context.bot.send_message(chat_id=update.effective_chat.id, text="You haven't created a character yet. Use /create to create one.")
         else:
+            # Prompt for selecting the price
+            keyboard = [
+                [InlineKeyboardButton("10 gold", callback_data=f'buy_price_{item_name}_10'),
+                 InlineKeyboardButton("20 gold", callback_data=f'buy_price_{item_name}_20')],
+                [InlineKeyboardButton("30 gold", callback_data=f'buy_price_{item_name}_30'),
+                 InlineKeyboardButton("Cancel", callback_data='buy_cancel')]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Please select the price:", reply_markup=reply_markup)
+
+def process_buy_callback(update, context):
+    query = update.callback_query
+    query.answer()
+    query_data = query.data
+
+    if query_data == 'buy_cancel':
+        context.bot.send_message(chat_id=query.message.chat_id, text="Purchase canceled.")
+    elif query_data.startswith('buy_price_'):
+        item_data = query_data.split('_')
+        item_name = item_data[2]
+        price = item_data[3]
+        character = context.user_data.get('character')
+        if character is not None:
             character.add_item_to_inventory(item_name)
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You bought {item_name}!")
+            character.add_gold(int(price))
+            context.bot.send_message(chat_id=query.message.chat_id, text=f"You bought {item_name} for {price} gold coins!")
 
 def gift(update, context):
     args = context.args
@@ -176,6 +239,9 @@ def main():
     dp.add_handler(CommandHandler("build", build))
     dp.add_handler(CommandHandler("hunt", hunt))
     dp.add_handler(CommandHandler("sell", sell))
+
+    # Add callback handlers
+    dp.add_handler(CallbackQueryHandler(process_buy_callback))
 
     # Add error handler
     dp.add_error_handler(error)
