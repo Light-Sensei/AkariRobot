694903b5041b39f5273697beb0a160147c669af1
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2023-06-17 18:23:59 +0530

    Update rpg.py

--- AkariRobot/modules/rpg.py
+++ AkariRobot/modules/rpg.py
@@ -104,9 +104,84 @@ name_callbacks = [
 ]
 
 
+# Command handler for /daily
+def daily(update: Update, context: CallbackContext):
+    # Generate a random reward for the daily command
+    rewards = ["100 gold coins", "a health potion", "a magic scroll"]
+    reward = random.choice(rewards)
+    
+    # Send the reward to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"You received {reward} as your daily reward!")
+
+# Command handler for /weekly
+def weekly(update: Update, context: CallbackContext):
+    # Generate a random reward for the weekly command
+    rewards = ["500 gold coins", "lucky item tap /spin to see", "a treasure map"]
+    reward = random.choice(rewards)
+    
+    # Send the reward to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"You received {reward} as your weekly reward!")
+
+# Command handler for /bal
+def balance(update: Update, context: CallbackContext):
+    # Get the user's balance from the database or any other storage
+    balance = 100  # Replace with the actual balance retrieval code
+    
+    # Send the user's balance to the chat
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your balance: {balance} gold coins")
+
+# Command handler for /inv
+def inventory(update: Update, context: CallbackContext):
+    # Get the user's inventory from the database or any other storage
+    items = ["Sword", "Shield", "Health Potion"]  # Replace with the actual inventory retrieval code
+    
+    # Format the items into a list
+    items_list = "\n".join(items)
+    
+    # Send the user's inventory to the chat
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your inventory:\n{items_list}")
+
+# Command handler for /hunt
+def hunt(update: Update, context: CallbackContext):
+    # Generate a random number to simulate the success of hunting
+    success = random.randint(0, 1)
+    
+    if success:
+        # User successfully hunted an animal
+        animal = random.choice(["rabbit", "deer", "wolf"])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You hunted a {animal}!")
+    else:
+        # User failed to hunt an animal and got injured
+        context.bot.send_message(chat_id=update.effective_chat.id, text="While hunting, you got injured. Try again later.")
+
+# Command handler for /build
+def build(update: Update, context: CallbackContext):
+    # Generate a random number to simulate the success of building
+    success = random.randint(0, 1)
+    
+    if success:
+        # User successfully built something
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You successfully built a house!")
+    else:
+        # User failed to build something
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Building construction failed. Try again later.")
+
+# Define command handlers
+
+
+# Add handlers to the dispatcher
+
+
+
+
 playrpg_handler = CommandHandler("playrpg", playrpg, run_async=True)
 create_handler = CommandHandler("create", create, run_async=True)
-
+daily_handler = CommandHandler("daily", daily, run_async=True)
+weekly_handler = CommandHandler("weekly", weekly, run_async=True)
+balance_handler = CommandHandler("bal", balance, run_async=True)
+inventory_handler = CommandHandler("inv", inventory, run_async=True)
+hunt_handler = CommandHandler("hunt", hunt, run_async=True)
+build_handler = CommandHandler("build", build, run_async=True)
 
 gender_callback_handler = CallbackQueryHandler(select_gender, pattern='^(male|female)$', run_async=True)
 name_callback_handler = CallbackQueryHandler(select_name, pattern='^(Jake|Zade|Josh|Aaron|Atlas|Mike|Jane|Lily|Julliete|Adeline|Grace|Olivia)$', run_async=True)
@@ -116,7 +191,12 @@ dispatcher.add_handler(playrpg_handler)
 dispatcher.add_handler(create_handler)
 dispatcher.add_handler(gender_callback_handler)
 dispatcher.add_handler(name_callback_handler)
-
+dispatcher.add_handler(daily_handler)
+dispatcher.add_handler(weekly_handler)
+dispatcher.add_handler(balance_handler)
+dispatcher.add_handler(inventory_handler)
+dispatcher.add_handler(hunt_handler)
+dispatcher.add_handler(build_handler)
 
 __mod_name__ = "RPG"
 __command_list__ = [
@@ -125,7 +205,13 @@ __command_list__ = [
 ]
 __handlers__ = [
     create_handler,
+    daily_handler,
+    weekly_handler,
+    balance_handler,
+    build_handler,
+    inventory_handler
     playrpg_handler,
+    hunt_handler,
     gender_callback_handler,
     name_callback_handler,
 ] 
